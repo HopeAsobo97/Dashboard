6af2777f9b7b05b100629f078cb0f2dbac0b48d2
HopeAsobo97 <awahope13@gmail.com>
2024-05-23 10:00:34 +0100

    new modifiation

--- src/Head.jsx
+++ src/Head.jsx
@@ -1,15 +1,19 @@
 import { ExpandMoreOutlined, LogoutOutlined, NotificationsOutlined, Search } from "@mui/icons-material";
 import { Avatar, Badge, Box, Button, Grid, ListItemText } from "@mui/material";
 import React from "react";
+import './responsiveness.css'
 
 const Head = () => {
+
+
+
   return (
-    <Box display={"flex"} justifyContent={'space-between'} alignItems={'center'} className="bg-white px-10 py-6">
-      <Grid display={"flex"} justifyContent={'center'} alignItems={'center'} item className="relative">
+    <Box display={"flex"} justifyContent={'space-between'} alignItems={'center'} className="bg-white header px-10 py-6">
+      <Grid display={"flex"} justifyContent={'center'} alignItems={'center'} item className="relative hide">
         <input type="search" placeholder="Search for a student, lecturer or course" className="border-slate-400 z-0 px-2 rounded-lg w-[350px] h-3 block py-[20px] text-sm border outline-1 outline-slate-500" name="" id="" />
         <Search style={{"fontSize":"24px"}} className="text-slate-400 absolute right-4"/>
       </Grid>
-      <Grid display={'flex'} className="gap-5" alignItems={'center'} item>
+      <Grid display={'flex'} className="gap-5 account-info" alignItems={'center'} item>
         <Button style={{'border':"1px solid black",'color':'black'}} variant="outlined" className="w-[25px] h-[30px]">En <ExpandMoreOutlined /></Button>
         <Badge color="error" badgeContent=" " variant="dot">
             <NotificationsOutlined />
--- src/Side.jsx
+++ src/Side.jsx
@@ -19,39 +19,39 @@ const Side = () => {
               <ListItemIcon><GridViewOutlined className='text-[#186318]'/></ListItemIcon> 
               <ListItemText className='text-[#186318] text-[11px]'>Dashboard</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><AccountBalanceOutlined/></ListItemIcon> 
               <ListItemText className='text-[11px]'>Schools</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><SupervisorAccountOutlined/></ListItemIcon> 
               <ListItemText className='text-[11px]'>Students</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><SchoolOutlined/></ListItemIcon> 
               <ListItemText className='text-[11px]'>Lectures</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><BookOutlined/></ListItemIcon> 
               <ListItemText className='text-[11px]'>Courses</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><BarChartOutlined /></ListItemIcon> 
               <ListItemText className='text-[11px]'>Results</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><SwapHorizOutlined /></ListItemIcon> 
               <ListItemText className='text-[11px]'>Finances</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><AccountCircleOutlined /></ListItemIcon> 
               <ListItemText className='text-[11px]'>Account</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><SettingsOutlined /></ListItemIcon> 
               <ListItemText className='text-[11px]'>Settings</ListItemText> 
             </ListItem> 
-            <ListItem> 
+            <ListItem className=' hover:bg-[#F0F1F3] w-[90%] rounded-s-full rounded-e-none  hover:text-[#186318] cursor-pointer'> 
               <ListItemIcon><LogoutOutlined className='text-[#D60A0B]' /></ListItemIcon> 
               <ListItemText className='text-[#D60A0B] text-[]'>Logout</ListItemText> 
             </ListItem> 
